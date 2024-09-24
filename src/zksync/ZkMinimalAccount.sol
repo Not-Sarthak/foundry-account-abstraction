ec514fcd8736ffd2bd7f3e163f7def0d2b3222f9
Sarthak Shah <sarthak@Sarthaks-MacBook-Pro.local>
2024-09-24 22:54:16 +0530

    feat: eth account abstraction

---
+++ src/zksync/ZkMinimalAccount.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {IAccount} from "lib/foundry-era-contracts/src/system-contracts/contracts/interfaces/IAccount.sol";
+
+contract ZkMinimalAccount is IAccount {
+    
+}
